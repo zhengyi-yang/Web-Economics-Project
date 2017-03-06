084cd387ad47631c29665df50f1cc2a1ad5aabcb
zhengyi-yang <zhengyi.yang@outlook.com>
2017-03-06 19:22:21 +0000

    bug fixed

--- src/bid.py
+++ src/bid.py
@@ -33,22 +33,26 @@ if __name__ == '__main__':
     import os
     import json
     from utils import dataloader, metrics
+    from collections import defaultdict
 
-    data = os.path.join('..', 'data','validation.csv')
+    data = os.path.join('..', 'data', 'validation.csv')
     out = os.path.join('..', 'out')
-    xs = range(1, 250)
+    xs = range(1, 300)
 
     if not os.path.exists(out):
         os.mkdir(out)
-
+        
+    const_results = defaultdict(dict)
+    rand_results = defaultdict(dict)
     for advertiser_id, df in dataloader(data).group():
-        const_results = {}
-        rand_results = {}
+        print advertiser_id
+        const_result=const_results[str(advertiser_id)]
+        rand_result=rand_results[str(advertiser_id)]
         for x in xs:
-            const_results[x] = (metrics(const_bid(df, x, 25000)).to_dict())
-            rand_results[x] = (metrics(rand_bid(df, x, 25000)).to_dict())
-    
-    with open(os.path.join(out,'const_bid.json'),'w') as f:
-        json.dump(const_results,f)
-    with open(os.path.join(out,'rand_bid.json'),'w') as f:
-        json.dump(rand_results,f)
+            const_result[x] = (metrics(const_bid(df, x, 25000)).to_dict())
+            rand_result[x] = (metrics(rand_bid(df, x, 25000)).to_dict())
+
+    with open(os.path.join(out, 'const_bid.json'), 'w') as f:
+        json.dump(const_results, f)
+    with open(os.path.join(out, 'rand_bid.json'), 'w') as f:
+        json.dump(rand_results, f)
