6e96856f7a7a6e09a24b92c22b63664f037ccc22
Neema <neema.kt@gmail.com>
2017-03-06 19:42:01 +0000

    results tables for const and rand for various bid limits.

---
+++ src/visualise.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from tabulate import tabulate
+from bid import const_bid, rand_bid
+from utils import dataloader, metrics
+
+
+def get_results_for_budget_limit(const_price, rand_upper, budget):
+    """Get results for budget limit."""
+    result_const = const_bid(dl.df, const_price, budget)
+    result_rand = rand_bid(dl.df, rand_upper, budget)
+
+    budget_limit = [budget // 2 ** i for i in range(0, 5)]
+    yc_metrics = []
+    yr_metrics = []
+
+    for bl in budget_limit:
+        result_const = const_bid(dl.df, const_price, budget)
+        result_rand = rand_bid(dl.df, 300, budget)
+
+        mtc = metrics(result_const)
+        mtr = metrics(result_rand)
+        yc_metrics.append([mtc.impressions, mtc.clicks, mtc.cost,
+                           mtc.CTR, mtc.CPM])
+        yr_metrics.append([mtr.impressions, mtr.clicks, mtr.cost,
+                           mtr.CTR, mtr.CPM])
+
+    headers = ['Imps', 'Clicks', 'Cost', 'CTR', 'CPM']
+    with open('../output/constant_bidding_budget_limit.txt', 'w+') as fd:
+        fd.write(''.join(tabulate(yc_metrics, headers=headers,
+                                  tablefmt='latex')))
+
+    with open('../output/random_bidding_budget_limit.txt', 'w+') as fd:
+        fd.write(''.join(tabulate(yr_metrics, headers=headers,
+                         tablefmt='latex')))
+
+
+if __name__ == '__main__':
+    dl = dataloader('../data/validation.csv')
+    mt = metrics(dl.df)
+
+    # just chose arbitrary values to see if it was working
+    get_results_for_budget_limit(100, 300, 25)
