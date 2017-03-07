be2330abf1dc3b2a4b7e92f0ec95aef7e695413c
Neema <neema.kt@gmail.com>
2017-03-07 18:51:19 +0000

    code for producing tables, have added numpy to requirements. Will likely remove it when I find same method in pandas

--- src/visualise.py
+++ src/visualise.py
@@ -1,45 +1,60 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import json
+import re
+import os, sys
+import numpy as np
 from tabulate import tabulate
-from bid import const_bid, rand_bid
 from utils import dataloader, metrics
+import matplotlib.pyplot as plt
 
 
-def get_results_for_budget_limit(const_price, rand_upper, budget):
-    """Get results for budget limit."""
-    budget_limit = [budget // 2 ** i for i in range(0, 5)]
-    yc_metrics = []
-    yr_metrics = []
+def produce_table(file_name, vis):
+    """Produce tables of results for constant and random bidding strategies."""
 
-    for bl in budget_limit:
-        result_const = const_bid(dl.df, const_price, budget)
-        result_rand = rand_bid(dl.df, 300, budget)
+    path_to_json = os.path.join('..', 'out', file_name)
+    with open(path_to_json) as f:
+        blob = f.read()
+    results = json.loads(blob)
 
-        mtc = metrics(result_const)
-        mtr = metrics(result_rand)
-        yc_metrics.append([bl, mtc.impressions, mtc.clicks, mtc.cost,
-                           mtc.CTR, mtc.CPM, mtc.CPC])
-        yr_metrics.append([bl, mtr.impressions, mtr.clicks, mtr.cost,
-                           mtr.CTR, mtr.CPM, mtr.CPC])
+    results_table = []
+    for adv in results.keys():
+        row = [adv, 0, 0, 0, 0, 0]
+        vals = results[adv]
+        for v in vals.values():
+            row[1] += v['impressions']
+            row[2] += v['clicks']
+            row[3] += v['cost']
+            if not np.isnan(v['CTR']):
+                row[4] += v['CTR']
+            if not np.isnan(v['CPM']):
+                row[5] += v['CPM']
+        results_table.append(row)
 
-    headers = ['Budget Limit', 'Imps', 'Clicks', 'Cost', 'CTR', 'CPM']
-    with open('../output/constant_bidding_budget_limit.txt', 'w+') as fd:
-        fd.write(''.join(tabulate(yc_metrics, headers=headers,
+    headers = ['Adv', 'Imps', 'Clicks', 'Cost', 'CTR', 'CPM']
+    name = re.sub(r"(.json)", '', file_name)
+
+    tables = os.path.join(vis, 'tables')
+    if not os.path.exists(tables):
+        os.mkdir(tables)
+
+    with open('%s/%s' % (tables, name), 'w+') as fd:
+        fd.write(''.join(tabulate(results_table, headers=headers,
                                   tablefmt='latex')))
 
-    with open('../output/random_bidding_budget_limit.txt', 'w+') as fd:
-        fd.write(''.join(tabulate(yr_metrics, headers=headers,
-                         tablefmt='latex')))
-
-
-def get_results_for_advertisers(const_price, rand_upper, budget):
-    """Get results for different advertisers."""
-    result_const = const_bid(dl.df, const_price, budget)
-    result_rand = rand_bid(dl.df, rand_upper, budget)
 
 if __name__ == '__main__':
     dl = dataloader('../data/validation.csv')
     mt = metrics(dl.df)
 
-    # just chose arbitrary values to see if it was working
-    get_results_for_budget_limit(100, 300, 25)
+    # this is where all plots and tables will go
+    vis = os.path.join('..', 'vis')
+
+    if not os.path.exists(vis):
+        os.mkdir(vis)
+
+    path = '../out'
+    out_files = ['const_bid_10000.json', 'const_bid_20000.json', 'const_bid_25000.json',
+                 'rand_bid_10000.json', 'rand_bid_20000.json', 'rand_bid_25000.json']
+
+    for out_file in out_files:
+        produce_table(out_file, vis)
