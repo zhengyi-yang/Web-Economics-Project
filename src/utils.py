4203975c3d9d424267d712115444bdefe040e9be
Zhengyi Yang <zhengyi.yang@outlook.com>
2017-03-17 02:20:25 +0000

    converting the data into sparse binary vectors

--- src/utils.py
+++ src/utils.py
@@ -10,13 +10,36 @@ import pandas as pd
 
 class dataloader(object):
 
-    def __init__(self, path):
+    def __init__(self, path, binarify=False):
         df = pd.read_csv(path)
-        try:
+        if 'bidprice' in df.columns and 'payprice' in df.columns:
             df = df[df.bidprice > df.payprice]
-        except Exception:
-            pass
         self.df = df
+        if binarify:
+            self._to_binary()
+
+    def _to_binary(self):
+        # ['weekday', 'hour', 'userid', 'useragent','region', 'city', 'domain','slotvisibility']
+        cols = ['weekday','useragent']
+        dummies = []
+        for name in cols:
+            dummy = pd.get_dummies(self.df[name], prefix=name)
+            del self.df[name]
+            dummies.append(dummy)
+
+        self.df.usertags = self.df.usertag.apply(
+            lambda x: set(x.split(',')) if x != 'null' else set())
+
+        tags = reduce(set.union, self.df.usertags)
+        dummy = pd.DataFrame(0, index=self.df.index, columns=tags)
+        for idx, usertag in self.df.usertags.iteritems():
+            dummy.loc[idx, usertag] = 1
+
+        del self.df.usertags
+        dummy.columns = ['usertags_' + str(col) for col in dummy.columns]
+        dummies.append(dummy)
+
+        self.df = pd.concat([self.df] + dummies, axis=1)
 
     def group(self):
         for advertiser_id in set(self.df.advertiser):
