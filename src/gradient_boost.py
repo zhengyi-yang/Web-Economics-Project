gradient boosting model - some bugs fixed

src/gradient_boost.py
@@ -8,7 +8,7 @@
 # python_version: 3.4.3
 # ==============================================================================
 
-from utils import dataloader
+from utils import dataloader, get_successful_bid
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import log_loss, mean_squared_error, roc_auc_score
@@ -17,10 +17,11 @@ import pandas as pd
 import os
 
 
-def gradientboost_strategy(path_to_training, path_to_test):
+def gradientboost_strategy(path_to_training, path_to_test, path_to_val):
     """Implement GBRT strategy."""
     training_set = dataloader(path_to_training)
     test_set = pd.read_csv(path_to_test)
+    val_set = pd.read_csv(path_to_val)
 
     # predictors for the model:
     # predictors = ['weekday', 'hour', 'userid', 'useragent', 'IP', 'region',
@@ -28,41 +29,44 @@ def gradientboost_strategy(path_to_training, path_to_test):
     #               'slotheight', 'slotvisibility', 'slotformat', 'slotprice',
     #               'creative', 'advertiser', 'usertag\n']
 
-    bid_prices, pctr = predict_bid_price(training_set, test_set, test_size=0.3)
+    bid_prices, pctr = predict_bid_price(training_set, test_set, val_set, cv=False)
     bidids = np.array(test_set.bidid.values)
     bidprices = np.array(bid_prices)
     bids = pd.DataFrame(np.column_stack((bidids, bidprices)),
                         columns=['bidid', 'bidprice'])
-    return (bids, pctr)
+
+    # Calculate the total spend
+    total = get_total_spend(val_set.payprice.values, bid_prices, 6250)
+    return (bids, pctr, total)
 
 
-def predict_bid_price(train, test, predictors=[], test_size=0.1, cv=True):
+def predict_bid_price(train, test, val, predictors=[], test_size=0.1, cv=True):
     """Predict the bid price using gradient boost classifier."""
-    y = train.df.click.values
-    X = train.df.drop(['click', 'bidprice', 'payprice', 'bidid', 'logtype'],
-                      axis=1)._get_numeric_data().values
+    size = len(train.df)
+    train.df = train.df.loc[np.random.choice(train.df.index, int(size // 100), replace=False)]
 
-    size = len(X)
-    split = int(round(size * (1 - test_size)))
+    y_train = train.df.click.values
+    X_train = train.df.drop(['click', 'bidprice', 'payprice', 'bidid', 'logtype'], axis=1)._get_numeric_data().values
 
-    # split into train and test sets
-    X_train, y_train = X[:split], y[:split]
-    X_test, y_test = X[split:], y[split:]
+    size_val = len(val)
+    y_test, y_val = val.click.values[:size_val//2], val.click.values[size_val//2:]
+    X_test, X_val = val.drop(['click', 'bidprice', 'payprice', 'bidid', 'logtype'],
+                             axis=1)._get_numeric_data().values[:size_val//2],\
+                    val.drop(['click', 'bidprice', 'payprice', 'bidid', 'logtype'],
+                             axis=1)._get_numeric_data().values[size_val//2:]
 
     # base bids is the bid price for the average CTR cases.
     prices = train.df.bidprice.values
-    base_bids = prices * 0.011 / prices.max(axis=0)
+    base_bids = prices * 0.9 / prices.max(axis=0)
 
     # select the best parameters
-    best_params = {}
+    best_params = {'subsample': 0.8, 'n_estimators': 5, 'min_samples_leaf': 11}
     if cv:
-        best_params = tune_model(X_train, y_train)
+        best_params = tune_model(X_val, y_val)
         print('Optimal parameter values: %s' % (str(best_params)))
 
     # retrain classifier with best parameters
-    clf = GradientBoostingClassifier(learning_rate=0.1, max_depth=3,
-                                     **best_params).fit(X_train, y_train)
-
+    clf = GradientBoostingClassifier(learning_rate=0.1, max_depth=3, **best_params).fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     auc_score = roc_auc_score(y_test, y_pred)
     logloss = log_loss(y_test, y_pred)
@@ -88,26 +92,36 @@ def predict_bid_price(train, test, predictors=[], test_size=0.1, cv=True):
 
 def tune_model(data, target):
     """Implement hyperparameter validation."""
-    param_grid = {'n_estimators': [25, 50, 75, 100],
-                  'min_samples_leaf': [13, 15, 17, 19, 21],
-                  'subsample': [0.5, 0.6, 0.7, 0.8, 1.0]
+    param_grid = {'n_estimators': [5, 10, 15, 20],
+                  'min_samples_leaf': [10, 11, 12, 13, 14, 15, 17],
+                  'subsample': [0.5, 0.6, 0.7, 0.8, 1.0],
                   }
-    clf = GradientBoostingClassifier()
+    clf = GradientBoostingClassifier(n_estimators=2)
     gs_cv = GridSearchCV(clf, param_grid).fit(data, target)
     return gs_cv.best_params_
+
+def get_total_spend(payprices, bidprices, budget=6250):
+    """ Calculate the total spend, cap at the budget price."""
+    total = 0
+    for pp, bp in zip(payprices, bidprices):
+        if bp > pp:
+            total += pp
+        if total > budget*1000:
+            break
+    return total // 1000
 
 
 if __name__ == '__main__':
     path_to_data = '../data'
     path_to_train = os.path.join(path_to_data, 'train.csv')
     path_to_test = os.path.join(path_to_data, 'test.csv')
-    # path_to_val = os.path.join(path_to_data, 'validation.csv')
+    path_to_val = os.path.join(path_to_data, 'validation.csv')
 
-    results, pctr = gradientboost_strategy(path_to_train, path_to_test)
+    results, pctr, total = gradientboost_strategy(path_to_train, path_to_test, path_to_val)
 
     with open('../out/gbdt_results.csv', 'w+') as res:
         results.to_csv(res)
 
-    print("""Predicted CTR: %f,\nNumber clicks: %d,\nTotal cost: %d,"""
-          % (np.mean(pctr), sum(pctr), sum(results.bidprice.values) / 1000)
+    print("""Predicted CTR: %f,\nNumber clicks: %d,\nTotal cost: %d CNY fen,"""
+          % (np.mean(pctr), sum(pctr), total)
           )